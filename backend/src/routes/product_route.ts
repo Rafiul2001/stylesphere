4da7949c6720d9c5b216e88d3c1c88149a0f0b37
Rafiul <rafiul13062001@gmail.com>
2025-07-19 18:55:00 +0600

    Updated all routes for produc

--- backend/src/routes/product_route.ts
+++ backend/src/routes/product_route.ts
@@ -65,13 +65,52 @@ product_router.post('/add-product', async (req: Request<{}, {}, Product>, res: R
 })
 
 // Update a Product By Id
-product_router.put('/:id', (req: Request, res: Response) => {
-    res.status(200).send(req.params.id)
+product_router.put('/:id', async (req: Request<{ id: string }, {}, { productPrice?: number, sizeWiseQuantity?: { size: string, quantity: number }[] }>, res: Response) => {
+    try {
+        const { productPrice, sizeWiseQuantity } = req.body
+        const updateResult = await database.collection<Product>(CollectionListNames.PRODUCT).findOneAndUpdate(
+            {
+                _id: new ObjectId(req.params.id)
+            },
+            {
+                $set: {
+                    ...(productPrice !== undefined && { productPrice }),
+                    ...(sizeWiseQuantity !== undefined && { sizeWiseQuantity })
+                }
+            },
+            { returnDocument: "after" } // to return updated document
+        )
+
+        if (!updateResult) {
+            return res.status(404).json({ message: "Product not found" })
+        } else {
+            res.status(200).json({
+                message: "Product is updated!",
+                value: updateResult
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: "Failed to add product",
+            error: (error as Error).message
+        })
+    }
 })
 
 // Delete a Product
-product_router.delete('/:id', (req: Request, res: Response) => {
-    res.status(200).send(req.params.id)
+product_router.delete('/:id', async (req: Request<{ id: string }, {}, {}>, res: Response) => {
+    try {
+        await database.collection<Product>(CollectionListNames.PRODUCT).deleteOne({ _id: new ObjectId(req.params.id) })
+        res.status(200).json({
+            message: `Product with ${req.params.id} deleted!`
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            message: "Failed to add product",
+            error: (error as Error).message
+        })
+    }
 })
 
 export default product_router
