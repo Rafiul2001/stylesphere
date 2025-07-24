7b6000f38b5b931f6f8d7be16ed4c16f2fc27c19
Rafiul <rafiul13062001@gmail.com>
2025-07-24 21:20:27 +0600

    Removed unnecessary code from cart model

--- backend/src/models/cart.ts
+++ backend/src/models/cart.ts
@@ -22,27 +22,4 @@ export class Cart {
         this.userId = userId
         this.cartItems = cartItems
     }
-
-    addItemsToCart(cartItem: CartItem) {
-        const existingItemIndex = this.cartItems.findIndex(item =>
-            item.productId === cartItem.productId &&
-            item.productSize === cartItem.productSize
-        );
-
-        if (existingItemIndex !== -1) {
-            // Item exists, update quantity and subtotal
-            const existingItem = this.cartItems[existingItemIndex];
-            existingItem.productQuantity += cartItem.productQuantity;
-        } else {
-            // New item, add to cart
-            this.cartItems.push(cartItem);
-        }
-    }
-
-    removeItemFromCart(productId: string, productSize: AvailableSizeKeys) {
-        this.cartItems = this.cartItems.filter(item =>
-            !(item.productId === productId && item.productSize === productSize)
-        );
-    }
-
 }
